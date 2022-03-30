a683c994350698e0570c32d89f921d39890d860a
Richard Austin <richardaustin123@icloud.com>
2022-03-30 17:07:45 +0100

    room with url

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ app.get('/:room', (req, res) => {
   res.render('room', { roomName: req.params.room })
 })
 
-server.listen(3000)
+server = http.Server(app);
+server.listen(process.env.PORT || 3000);
 
 io.on('connection', socket => {
   socket.on('new-user', (room, name) => {
